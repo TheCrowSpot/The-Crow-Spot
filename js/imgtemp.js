312600123759a021b34b6c541e8afb80fa70d203
Alex <thecrowspot.site@gmail.com>
2025-03-11 22:29:47 -0300

    test (revert if unused)

---
+++ js/imgtemp.js
@@ -0,0 +1,12 @@
+
+function showImg( id ) {
+// hide previously shown image
+for ( i = 1; i < 10; i++) {
+    var obj = document.getElementById( "picture" + i );
+    if (obj != null)
+        obj.className = 'hide';
+}
+var obj = document.getElementById( "picture" + id );
+if (obj != null)
+    obj.className = 'show';
+  }
